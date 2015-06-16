46523bc68e50208cb3f27a1d2af43341b3d7c0b1
System Administrator <root@zhanghengyangdeMacBook-Pro-2.local>
2015-06-17 00:18:07 +0800

    reset the author

--- node_.js
+++
@@ -1,9 +0,0 @@
-var publisher = require('./essential_modules/rabbit_lib/publisher');
-
-[1,2,3,4,4,5,6,7,7,7,8,8,9,99,9,9,9,9,].forEach(function(id){
-    var msg = {
-        'objectId': id,
-        'timestamp': Date.now()
-    };
-    publisher.publishMessage(msg, 'new_motion_arrival');
-});
