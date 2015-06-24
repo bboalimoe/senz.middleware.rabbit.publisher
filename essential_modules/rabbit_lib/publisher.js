11658cbc486f25c8f1b8713d1149cdd83fc40e53
zhanghengyang <danielzhang-1@163.com>
2015-06-24 19:29:54 +0800

    fix the rabbit mq bug

--- essential_modules/rabbit_lib/publisher.js
+++ essential_modules/rabbit_lib/publisher.js
@@ -3,6 +3,16 @@ var configuration = require('./configuration.js');
 var log = require("../utils/logger").log;
 var logger = new log("rabbitmq pub");
 
+
+var env = null;
+if(process.env.APP_ENV === "prod"){
+    env = "_prod"
+
+}else if(process.env.APP_ENV === "test"){
+
+    env = "_test"
+}
+
 publishMsg = function(msg, event) {
     logger.info("",'------ Sending ------');
     logger.info("",'* The chosen event is ' + event + '\n* The content of Msg is ' + msg + '\n* Sending Msg...\n');
@@ -10,9 +20,9 @@ publishMsg = function(msg, event) {
     if(event == "new_sound_arrival"){var routing_key = "sound";}
     if(event == "new_location_arrival"){var routing_key = "location";}
     //not right
-    var type = "senz.message." + routing_key;
+    var type = "senz.message." + routing_key + env;
     logger.info("","event is " + type);
-    rabbit.publish(event, {
+    rabbit.publish(event + env, {
         type: type,
         body: msg
         //routingKey: routing_key
